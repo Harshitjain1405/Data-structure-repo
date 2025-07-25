519d88f0e970827fc53443e8043c1b1c5401ca49
hjain <hjain1405@gmail.com>
2025-07-25 10:40:48 +0530

    adding more programs

---
+++ PatternProgram/src/general/InsertInterval.java
@@ -0,0 +1,67 @@
+package general;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+Return intervals after the insertion.
+
+Note that you don't need to modify intervals in-place. You can make a new array and return it.
+
+
+
+Example 1:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+Example 2:
+
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+ */
+public class InsertInterval {
+    public static void main(String[] args) {
+        int[][] intervals = new int[][]{
+                {1,3},
+                {6,9}
+        };
+
+        int[] newInterval = new int[]{2,5};
+
+        int[][] resultantInterval = insertInterval(intervals, newInterval);
+
+        System.out.println(Arrays.deepToString(resultantInterval));
+    }
+
+    private static int[][] insertInterval(int[][] intervals, int[] newInterval) {
+        int n = intervals.length;
+
+        List<int[]> resultantList = new ArrayList<>();
+        int i = 0;
+
+        while(i < n && intervals[i][1] < newInterval[0]) {
+            resultantList.add(intervals[i]);
+            i++;
+        }
+
+        while(i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        resultantList.add(newInterval);
+
+        while(i  < n) {
+            resultantList.add(intervals[i]);
+            i++;
+        }
+
+        return resultantList.toArray(new int[resultantList.size()][]);
+    }
+}
