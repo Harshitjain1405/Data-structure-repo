22d9c4b94dd3030100a193a95b70609015366cdd
hjain <hjain1405@gmail.com>
2025-07-31 08:44:57 +0530

    adding more programs

--- PatternProgram/src/general/CountDuplicate2.java
+++ PatternProgram/src/general/CountDuplicate2.java
@@ -5,10 +5,7 @@ import java.util.Map;
 
 /*
 219. Contains Duplicate II
-Easy
-Topics
-premium lock icon
-Companies
+
 Given an integer array nums and an integer k, return true if
 there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
 
